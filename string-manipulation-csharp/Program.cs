12b6495b5b9dde3de5aac16ab9239b05945377af
trsaints <thiago.dev2k3@gmail.com>
2024-02-10 21:30:17 -0300

    Testing IndexOf and Substring methods

--- string-manipulation-csharp/Program.cs
+++ string-manipulation-csharp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Insert the contents of your HTML: ");
+string? message = Console.ReadLine();
+
+Console.Write("Insert the HTML tag name: ");
+string tagName = Console.ReadLine();
+
+while (tagName.Length == 0)
+{
+    Console.Write("Null entries are not allowed. Insert the HTML tag name: ");
+    tagName = Console.ReadLine();
+}
+
+string generatedTag = GenerateHTMLTagWithContent(message, tagName);
+
+ExtractHTMLContent(tagName, generatedTag);
+
+string GenerateHTMLTagWithContent(string? content, string tagName)
+{
+    string tag = $"<{tagName}>{content}</{tagName}>";
+    Console.WriteLine($"Generated HTML: {tag}");
+
+    return tag;
+}
+
+string ExtractHTMLContent(string tagName, string HTMLTag)
+{
+    string startTag = $"<{tagName}>",
+        endTag = $"</{tagName}>";
+
+    int startTagIndex = HTMLTag.IndexOf(startTag),
+        endTagIndex = HTMLTag.IndexOf(endTag),
+        contentLength = endTagIndex - (startTagIndex + startTag.Length);
+
+    string content = HTMLTag.Substring((startTagIndex + startTag.Length), contentLength);
+
+    Console.WriteLine($"Content length: {contentLength}\nExtracted content: {content}");
+
+    return content;
+}
